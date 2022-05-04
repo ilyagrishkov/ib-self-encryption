df8229f42c0c89807b8056c3ef14711644e9afe8
Ilya Grishkov <ilyagrishkov@icloud.com>
2022-05-04 21:02:11 +0100

    Working encrypt/decrypt

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	wasm := rustgo.NewWasmLib("rustgo/id_based_self_encryption.wasm")
+	wasm := rustgo.NewWasmLib("rustgo/ib_self_encryption_rust.wasm")
 	defer wasm.Close()
 
 	_, _ = wasm.Invoke("self_encrypt", rustgo.Void, "test.txt")
+
+	_, _ = wasm.Invoke("self_decrypt", rustgo.Void)
 }
